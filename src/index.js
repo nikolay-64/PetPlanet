28e8d29e6d79af96556ee84a7c6203ecc02f4b78
nikolay-64 <kolyavyazmin@yandex.ru>
2024-09-11 20:28:07 +0400

    project upgrade added product api and updated shopping cart styles

--- src/index.js
+++ src/index.js
@@ -13,3 +13,58 @@ const changeActiveBtn = (event) => {
 buttons.forEach((button) => {
 	button.addEventListener('click', changeActiveBtn);
 });
+
+const API_URL = 'https://lush-hip-hexagon.glitch.me';
+// /api/products/category
+
+const productList = document.querySelector('.store__list');
+
+const createProductCard = (product) => {
+	// console.log("product: ", product);
+	const productCard = document.createElement('store__itemli');
+	productCard.classList.add('store__item');
+
+	productCard.innerHTML = `
+		<article class="store__product product">
+			<img class="product__image" src="${API_URL}${product.photoUrl}" 
+				alt="${product.name}" width="388" height="261">
+
+			<h3 class="product__title">${product.name}</h3>
+
+			<p class="product__price">${product.price}&nbsp;₽</p>
+
+			<button class="product__button-add-cart">Заказать</button>
+		</article>
+	`;
+
+	return productCard;
+};
+
+const renderProducts = (products) => {
+	productList.textContent = '';
+	products.forEach((product) => {
+		const productCard = createProductCard(product);
+
+		productList.append(productCard);
+	});
+};
+
+const fetchProductByCategory = async (category) => {
+	try {
+		const response = await fetch(
+			`${API_URL}/api/products/category/${category}`
+		);
+
+		if (!response.ok) {
+			throw new Error('response.status');
+		}
+
+		const products = await response.json();
+
+		renderProducts(products);
+	} catch (error) {
+		console.error(`Ошибка запроса товаров:${error}`);
+	}
+};
+
+fetchProductByCategory('Домики');
